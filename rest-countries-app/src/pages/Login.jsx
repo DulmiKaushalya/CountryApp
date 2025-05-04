1f694ab7861b956b301b57dbf0219ea1ef582662
DulmiKaushalya <146383047+DulmiKaushalya@users.noreply.github.com>
2025-05-04 16:02:56 +0530

    Update Login.jsx

--- rest-countries-app/src/pages/Login.jsx
+++ rest-countries-app/src/pages/Login.jsx
@@ -20,7 +20,7 @@ export default function Login() {
     setError("");
     
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("https://countryapp-2.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
